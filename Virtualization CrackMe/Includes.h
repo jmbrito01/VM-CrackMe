d5ae9deaab9517a7540b40a75334e2c802187670
João Marcelo Brito <jmbrito1996@hotmail.com>
2015-04-05 05:28:02 -0300

    Version 0.0.1
    Code Virtualizer written.

---
+++ Virtualization CrackMe/Includes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#include <Windows.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
---
+++ Virtualization CrackMe/Main.cpp
@@ -0,0 +1,8 @@
+#include "Includes.h"
+
+int main(int argc, char** argv)
+{
+	cout << argc << endl;
+	system("PAUSE");
+	exit(0);
+}
