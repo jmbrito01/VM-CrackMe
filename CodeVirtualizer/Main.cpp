4326187b41b4de6ded98b7e144aecbe7c7db5699
João Marcelo Brito <jmbrito1996@hotmail.com>
2015-04-13 16:02:48 -0300

    Virtualizer classes build.

--- CodeVirtualizer/Main.cpp
+++ CodeVirtualizer/Main.cpp
@@ -1,3 +1,6 @@
+#define D_SCL_SECURE_NO_WARNINGS
+
+
 #include <Windows.h>
 #include <iostream>
 #include <string>
@@ -5,6 +8,8 @@
 #include <memory>
 #include <fstream>
 #include <sstream>
+
+
 
 using namespace std;
 
@@ -26,28 +31,31 @@ vector<string> split(const string &s, char delim) {
 //##############################################################################################
 //[ DEFINES ]
 #define INITIAL_STUB_SIZE						10000
-#define INITIAL_STACK_SIZE						00500
-//[ OPCODES ]
-#define OPCODE_SET								0x01
 //##############################################################################################
 //[ STRUCTURES ]
 struct VMFunction
 {
-	VMFunction() {}
-	VMFunction(string n, DWORD o, DWORD s): Name(n), Offset(o), Size(s) {}
-	string Name;
+public:
+	char Name[25];
 	DWORD Offset;
 	DWORD Size;
-};
-struct VMRegisters
-{
-	DWORD	regs[6];
-};
-struct VMConst
-{
-	DWORD Offset;
-	DWORD Size;
-	//BYTE Data[]; 
+	static VMFunction VMFunction::Init(char* n, DWORD o, DWORD s)
+	{
+		VMFunction func;
+		func.Offset = o;
+		func.Size = s;
+		ZeroMemory(func.Name, 25);
+		memcpy((void*)func.Name, (void*)n, strlen(n));
+		return func;
+	}
+	static VMFunction Init()
+	{
+		VMFunction func;
+		func.Offset = 0;
+		func.Size = 0;
+		ZeroMemory(func.Name, 25);
+		return func;
+	}
 };
 //##############################################################################################
 //[ ENUMERATORS ]
@@ -67,9 +75,9 @@ class VMFunctionSystem
 {
 public:
 	VMFunctionSystem() {}
-	virtual void AddFunction(string Name, DWORD Offset, DWORD Size)
+	virtual void AddFunction(char* Name, DWORD Offset, DWORD Size)
 	{
-		Functions.push_back(VMFunction(Name, Offset, Size));
+		Functions.push_back(VMFunction::Init(Name, Offset, Size));
 	}
 	virtual void AddFunction(VMFunction func)
 	{
@@ -90,10 +98,26 @@ public:
 	{
 		for (int i = 0; i < Functions.size(); i++)
 		{
-			if (Functions[i].Name.compare(lpFunction) == 0)
+			if (strcmp(lpFunction.c_str(), Functions[i].Name) == 0)
 				return i;
 		}
 		return -1;
+	}
+	virtual void Export(string lpFile)
+	{
+		HANDLE hFile = CreateFile(lpFile.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (hFile == INVALID_HANDLE_VALUE)
+		{
+			cout << "[ ERROR ] EXPORTING FUNCTIONS" << endl;
+			return;
+		}
+		VMFunction* f = new VMFunction[Functions.size()];
+		for (int i = 0; i < Functions.size(); i++)
+		{
+			DWORD Written;
+			WriteFile(hFile, (void*)&Functions[i], sizeof(VMFunction), &Written, NULL);
+		}
+		CloseHandle(hFile);
 	}
 private:
 	vector<VMFunction> Functions;
@@ -104,7 +128,15 @@ public:
 	VMData() {}
 	virtual BYTE appendData(char* Data)
 	{
-		data.push_back(string(Data));
+		char* newData = (char*)VirtualAlloc(NULL, strlen(Data) + 1, MEM_COMMIT, PAGE_READWRITE);
+		ZeroMemory((void*)newData, strlen(Data) + 1);
+		for (int i = 0; i < strlen(Data); i++)
+		{
+			if (Data[i] == 0xFF)
+				newData[i] = 0;
+			newData[i] = Data[i] + (char)1;
+		}
+		data.push_back(string(newData));
 		return data.size() - 1;
 	}
 	virtual void buildFile(string filename)
@@ -277,13 +309,13 @@ shared_ptr<VMMacroSystem> MacroSystem;
 shared_ptr<VMOutput> Output;
 shared_ptr<VMData> Data;
 //##############################################################################################
-
 int main(void)
 {
 	cout << "Please type the file containing the code: " << endl;
 	string filename;
 	cin >> filename;
 	cout << filename << endl;
+	cout << "Compiling..." << endl;
 	HANDLE hFile = CreateFile(filename.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
@@ -306,27 +338,26 @@ int main(void)
 	//START PARSING THE FILES
 	//PARSING FUNCTIONS
 	
-	VMFunction new_func;
+	VMFunction new_func = VMFunction();
 	string lpCode = string((char*)pAlloc);
 	VirtualFree(pAlloc, dwSize + 1, MEM_DECOMMIT);
-	DWORD CurOffset;
+	DWORD CurOffset = 0;
 	void* pStub = malloc(INITIAL_STUB_SIZE);
 	ZeroMemory(pStub, INITIAL_STUB_SIZE);
 	int location = lpCode.find("#");
-	cout << "#############################" << endl;
+
 	while (location != lpCode.npos)
 	{
+		ZeroMemory((void*)&new_func, sizeof(VMFunction));
 		int end_name = lpCode.find(":", location);
-		new_func.Name = lpCode.substr(location + 1, end_name-location-1);
+		memcpy(new_func.Name, lpCode.substr(location + 1, end_name - location - 1).c_str(), lpCode.substr(location + 1, end_name - location - 1).length());
+		new_func.Offset = CurOffset;
 
 		int start_func = strlen("__begin__") + lpCode.find("__begin__", location + 1)+1;
 		int end_func = lpCode.find("__end__", start_func)-start_func;
 		string lpFunction = lpCode.substr(start_func, end_func);
-		
-		cout << "#############################" << endl;
 
-
-		//PASSING EACH FUNCTION
+		//PASSING EACH COMMAND
 		int hash_call = lpFunction.find(">");
 		while (hash_call != lpFunction.npos && hash_call < end_func)
 		{
@@ -346,10 +377,13 @@ int main(void)
 			}
 		}
 		FuncSystem->AddFunction(new_func);
-		cout << "[ END OF FUNCTION " << new_func.Name << " ]" << endl;
+		CurOffset += new_func.Size;
 		location = lpCode.find("#", location + 1);
 	}
+	cout << "Building DATA Stub..." << endl;
 	Data->buildFile("C:\\Users\\jmbri\\data.stub");
+	cout << "Building exports..." << endl;
+	FuncSystem->Export("C:\\Users\\jmbri\\export.stub");
 	system("PAUSE");
 	exit(0);
 }
